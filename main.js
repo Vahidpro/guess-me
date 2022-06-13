400b688378e8ba9ec7dc7a152a4bae4de50d25f5
Vahid Porki <ivahidp@gmail.com>
2022-06-13 23:10:40 +0430

    Add modal and overlay for Guide section

--- main.js
+++ main.js
@@ -6,6 +6,11 @@ const shareData = {
 	url: "https://letsguessme.netlify.app/",
 };
 
+// Functions
+const querySelector = function (className) {
+	return document.querySelector("className");
+};
+
 // Letters Counter
 const wordInput = document.querySelector(".word-input");
 const log = document.querySelector(".letters-counter");
@@ -15,3 +20,37 @@ wordInput.addEventListener("input", updateLetters);
 function updateLetters(e) {
 	log.textContent = e.target.value.length;
 }
+
+// Modal
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close");
+const btnsOpenModal = document.querySelector(".guide");
+
+const openModal = function () {
+	overlay.style.display = "block";
+	modal.style.display = "block";
+
+	setTimeout(function () {
+		overlay.classList.add("open");
+		modal.classList.add("open");
+	}, 10);
+};
+
+const closeModal = function () {
+	overlay.classList.remove("open");
+	modal.classList.remove("open");
+	setTimeout(function () {
+		overlay.style.display = "none";
+		modal.style.display = "none";
+	}, 500);
+};
+
+btnsOpenModal.addEventListener("click", openModal);
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+document.addEventListener("keydown", function (e) {
+	if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+		closeModal();
+	}
+});
